bd5b7e4db84d779f9ca7de74459eefa90e782de3
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2025-05-05 14:32:40 +0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv'
+import connectDB from './config/db'
+dotenv.config()
+
+import app from './server'
+
+const PORT = process.env.PORT || 4000
+
+const startServer = async () => {
+  await connectDB()
+  app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`)
+  })
+}
+
+startServer()
