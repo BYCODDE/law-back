469be103e90001f7716f3324f06860cdeedc5305
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2025-05-06 19:08:10 +0400

    auth register and login logics added.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import authRoutes from './routes/auth'
 
 const app = express()
 
@@ -8,5 +9,7 @@ app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
+
+app.use('/api/auth', authRoutes)
 
 export default app
